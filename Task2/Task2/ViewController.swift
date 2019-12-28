54b8ef370311f8cbbaa3f55642566c94378827bb
Andrii Voitenko <voandrij@gmail.com>
2019-12-28 21:25:55 +0200

    Initial Commit

---
+++ Task2/Task2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by Andrii on 12/28/19.
+//  Copyright © 2019 Andrii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
